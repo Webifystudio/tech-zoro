b2ef5d65f4a4d39147f56a2f11ac8f5d9e924ac5
XP NETWORK <xpnetwork.tech@gmail.com>
2025-07-12 05:03:11 +0000

    Add this api key AIzaSyDat9BP3QffC2hzxWAEeGrKNEeaWejteK4

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { getAuth, type Auth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPDoh0znVAGCKNav2qX9gqh4eVGSoDLi0",
+  apiKey: "AIzaSyDat9BP3QffC2hzxWAEeGrKNEeaWejteK4",
   authDomain: "tech-zoro.firebaseapp.com",
   databaseURL: "https://tech-zoro-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tech-zoro",
-  storageBucket: "tech-zoro.firebasestorage.app",
+  storageBucket: "tech-zoro.appspot.com",
   messagingSenderId: "588736971823",
   appId: "1:588736971823:web:571ca28714cba8136032da",
   measurementId: "G-8L19QD2GKM"
